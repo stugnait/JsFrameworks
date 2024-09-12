b4f4783038bab09fa8d1e6fb35adf5d1aa3bdb5a
stugnait <teslitskii.link@gmail.com>
2024-09-12 18:04:33 +0300

    lab 3 init

---
+++ lr3/lab-app/src/app.ts
@@ -0,0 +1,13 @@
+// import all modules here
+import {Book, User} from './models';
+import {LibraryService} from './services';
+
+// etc.
+
+class App {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    new App();
+});
